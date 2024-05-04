89a17c052bedcf1a7764ec7896be54ba726afd8d
akkunov <akunov313131@gmail.com>
2024-05-04 16:27:58 +0600

    полностю переделал меню

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 document.addEventListener('DOMContentLoaded', function(){
     const Burger = document.querySelector('.burger'),
-    headerContainer = document.getElementById('headerContent'),
     nav = document.getElementById('navigationMenu');
     Burger.addEventListener('click' ,function() {
-        headerContainer.classList.toggle('open');
+        Burger.classList.toggle('open')
         nav.classList.toggle('open');
-        Burger.classList.toggle('open');
     })
 })
